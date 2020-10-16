aca6bd743244370976c3579f0a908ce32737d586
thunderstorm010 <yigitozdem3@gmail.com>
2020-10-16 15:10:27 +0300

    Initial commit

--- src/main/kotlin/me/thunderstorm/api/vacefron/VACEfron.kt
+++ src/main/kotlin/me/thunderstorm/api/vacefron/VACEfron.kt
@@ -14,6 +14,7 @@ import java.nio.charset.StandardCharsets
 
 class VACEfron : ApiUtil() {
     override var baseUrl = "https://vacefron.nl/api/"
+    override var userAgent: String = "VACEfron.kt"
 
     override fun parseErr(body: ResponseBody) {
         val obj = Json.parse(body.string()).asObject()
